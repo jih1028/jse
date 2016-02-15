f9ca3fd885adb933f610440d94cc733e81fc9095
user <user@192.168.10.38>
2016-02-15 14:21:06 +0900

    .

--- jse/src/operation/ReportCard.java
+++ jse/src/operation/ReportCard.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class ReportCard {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		//학생이름
+		// 학생이름
 		System.out.println("학생 이름:");
 		String name = scanner.next();
-		
-		//국영수과, 점수 입력 받기
-		System.out.println("국어점수:");		
+
+		// 국영수과, 점수 입력 받기
+		System.out.println("국어점수:");
 		int kor = scanner.nextInt();
 		System.out.println("영어점수:");
 		int eng = scanner.nextInt();
@@ -20,13 +20,13 @@ public class ReportCard {
 		int math = scanner.nextInt();
 		System.out.println("과학점수:");
 		int sci = scanner.nextInt();
-		
-		//총점 얼마?, 평균 얼마?
+
+		// 총점 얼마?, 평균 얼마?
 		int total = 0;
 		double avg = 0.0;
-		total = kor + eng+math+sci;
-		avg = total/4;
-		System.out.println("총점:" + total + "     평균:"+avg);
+		total = kor + eng + math + sci;
+		avg = total / 4;
+		System.out.println("총점:" + total + "     평균:" + avg);
 
 	}
 
